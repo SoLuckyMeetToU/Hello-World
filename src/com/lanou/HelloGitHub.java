f8d03c7f880a19a78064bab1166028047e89fa4f
qiao <qiao@So-Ly-Mt-To-U>
2019-02-19 19:40:15 +0800

    1111

--- src/com/lanou/HelloGitHub.java
+++ src/com/lanou/HelloGitHub.java
@@ -3,7 +3,7 @@ package com.lanou;
 public class HelloGitHub {
 	public static void main(String[] args) {
 		System.out.println("hello GitHub");
-	
+		System.out.println("aa");
 		System.out.println("test");
 		
 	}
